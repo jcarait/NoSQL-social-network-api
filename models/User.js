ea4b0e60dfa8ee76f0fe719fd6ce879ddf8d1e17
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-04-14 15:21:29 +1000

    setup files and folders, created user model WIP

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+const { stringify } = require('querystring');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+    }
+  }
+)
