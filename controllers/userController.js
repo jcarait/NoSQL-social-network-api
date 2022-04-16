ad1f486a75ae4b63fc1f88a9472133307bcc5273
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-04-16 18:41:54 +1000

    setup route for getting single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,26 @@
 const User = require('../models/User');
 
 module.exports = {
+  //Get all users
   getUsers(req, res) {
     User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(500).json(err));
+  },
+  //Get a  single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then(async (user) =>
+        !user 
+          ? res.status(404).json({ message: 'No student with that ID' }) 
+          : res.json({
+              user,
+          })
+    )
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json(err);
+    });
   },
 };
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 const {
     getUsers,
+    getSingleUser
 } = require('../../controllers/userController.js')
 
 // /api/users
 router.route('/').get(getUsers)
+
+router.route('/:userId').get(getSingleUser)
 
 module.exports = router;
